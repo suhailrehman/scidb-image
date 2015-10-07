53a3b776ddf617c97ffdbe44ca90149230885a37
scidb <suhailrehman@gmail.com>
2015-10-07 00:50:49 -0400

    First set of results ready for evaluation...
    Added a number of datasets.

--- weekly/2015_10_7/image_average.py
+++ weekly/2015_10_7/image_average.py
@@ -7,6 +7,8 @@ import sys
 import os
 import numpy
 
+import timer
+
 #Helper function to get 
 def absoluteFilePaths(directory):
    for dirpath,_,filenames in os.walk(directory):
@@ -16,22 +18,33 @@ def absoluteFilePaths(directory):
 #Default Values
 OUTPUT_FILE = 'output.jpg'
 HANDLE_OVERFLOW = True
+TIMING_INFO = ''
 
 #Connect to localhost SciDB instance thorugh shim layer
-sdb = connect('http://localhost:8080/')
+with timer.Timer() as t:
+	sdb = connect('http://localhost:8080/')
+TIMING_INFO+='Initiating SciDB connection :%.03f' % t.interval+' sec\n'
 
 #Use PIL 
 io.use_plugin('pil')
 
 def read_image_into_scidb(img_file):
+	global TIMING_INFO
 	print 'Reading: '+img_file
-	image_array = io.imread(img_file)
 
-	#Store image in SciDB
-	if HANDLE_OVERFLOW: #Extend to 32-bit int for summation
-		image_array_sdb = sdb.from_array(image_array.astype(numpy.uint32))
-	else: #Don't care, let it overflow.
-		image_array_sdb = sdb.from_array(image_array)
+	with timer.Timer() as t:
+		image_array = io.imread(img_file)
+	TIMING_INFO+='Reading image file: '+img_file+' :%.03f' % t.interval+' sec\n'
+
+	with timer.Timer() as t:
+		#Store image in SciDB
+		if HANDLE_OVERFLOW: #Extend to 32-bit int for summation
+			image_array_sdb = sdb.from_array(image_array.astype(numpy.uint32))
+		else: #Don't care, let it overflow.
+			image_array_sdb = sdb.from_array(image_array)
+
+	TIMING_INFO+='Storing image file in DB: '+img_file+' :%.03f' % t.interval+' sec\n'
+		
 
 	return image_array_sdb
 
@@ -65,7 +78,10 @@ if __name__ == "__main__":
 
 	#Get the schema for the first image and create the result image in SciDB:
 	print 'Getting dimensions of first image in directory...'
-	sum_image = generate_scidb_result_array(next(image_files))
+	with timer.Timer() as t:
+		sum_image = generate_scidb_result_array(next(image_files))
+	TIMING_INFO+='Reading first file and generating empty array :%.03f' % t.interval+' sec\n'
+
 
 	count = 0
 	#for each file in the source directory
@@ -74,13 +90,29 @@ if __name__ == "__main__":
 		image_array_sdb = read_image_into_scidb(img_file)
 		
 		#TODO: Image dimension validation here
-		sum_image = sum_image + image_array_sdb
-		image_array_sdb.reap() #Reap this image
+		with timer.Timer() as t:
+			sum_image = sum_image + image_array_sdb
+		TIMING_INFO+='Adding image file to Sum: '+img_file+' :%.03f' % t.interval+' sec\n'
+		with timer.Timer() as t:
+			image_array_sdb.reap() #Reap this image
+		TIMING_INFO+='Reaping Image: '+img_file+' :%.03f' % t.interval+' sec\n'
+
 		count+=1
 
 	#Divide by total # of images to get average
-	sum_image = sum_image / count
-	result_array = sum_image.toarray()
+	with timer.Timer() as t:
+		sum_image = sum_image / count
+	TIMING_INFO+='Scalar division on result array :%.03f' % t.interval+' sec\n'
 
-	io.imsave(OUTPUT_FILE, result_array.astype(numpy.uint8))
+	with timer.Timer() as t:
+		result_array = sum_image.toarray()
+	TIMING_INFO+='Fetching result array from SciDB :%.03f' % t.interval+' sec\n'
+
+	with timer.Timer() as t:
+		io.imsave(OUTPUT_FILE, result_array.astype(numpy.uint8))
+	TIMING_INFO+='Writing final output to disk :%.03f' % t.interval+' sec\n'
+
 	print 'Output written to: '+OUTPUT_FILE
+
+	print '\nTiming Info:\n============'
+	print TIMING_INFO
--- weekly/2015_10_7/timer.py
+++ weekly/2015_10_7/timer.py
@@ -2,9 +2,9 @@ import time
 
 class Timer:    
     def __enter__(self):
-        self.start = time.clock()
+        self.start = time.time()
         return self
 
     def __exit__(self, *args):
-        self.end = time.clock()
+        self.end = time.time()
         self.interval = self.end - self.start
