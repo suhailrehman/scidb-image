d666737bd3099cddec196f02e70932702c954d81
scidb <suhailrehman@gmail.com>
2015-11-05 17:53:08 -0500

    SciDB UDO directory with working Makefile for my laptop

---
+++ scidb_udos/CMakeFiles/example_udos.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/scidb/scidb/scidb-eclipse/examples/example_udos/HelloInstances/LogicalHelloInstances.cpp" "/home/scidb/scidb/scidb-eclipse/examples/example_udos/CMakeFiles/example_udos.dir/HelloInstances/LogicalHelloInstances.cpp.o"
+  "/home/scidb/scidb/scidb-eclipse/examples/example_udos/HelloInstances/PhysicalHelloInstances.cpp" "/home/scidb/scidb/scidb-eclipse/examples/example_udos/CMakeFiles/example_udos.dir/HelloInstances/PhysicalHelloInstances.cpp.o"
+  "/home/scidb/scidb/scidb-eclipse/examples/example_udos/InstanceStats/LogicalInstanceStats.cpp" "/home/scidb/scidb/scidb-eclipse/examples/example_udos/CMakeFiles/example_udos.dir/InstanceStats/LogicalInstanceStats.cpp.o"
+  "/home/scidb/scidb/scidb-eclipse/examples/example_udos/InstanceStats/PhysicalInstanceStats.cpp" "/home/scidb/scidb/scidb-eclipse/examples/example_udos/CMakeFiles/example_udos.dir/InstanceStats/PhysicalInstanceStats.cpp.o"
+  "/home/scidb/scidb/scidb-eclipse/examples/example_udos/plugin.cpp" "/home/scidb/scidb/scidb-eclipse/examples/example_udos/CMakeFiles/example_udos.dir/plugin.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "include"
+  "utils"
+  "/opt/scidb/15.7/3rdparty/boost/include"
+  "/usr/include/google/protobuf"
+  "/usr/include/postgresql"
+  "extern"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ scidb_udos/CMakeFiles/example_udos.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/example_udos.dir/plugin.cpp.o"
+  "CMakeFiles/example_udos.dir/HelloInstances/LogicalHelloInstances.cpp.o"
+  "CMakeFiles/example_udos.dir/HelloInstances/PhysicalHelloInstances.cpp.o"
+  "CMakeFiles/example_udos.dir/InstanceStats/LogicalInstanceStats.cpp.o"
+  "CMakeFiles/example_udos.dir/InstanceStats/PhysicalInstanceStats.cpp.o"
+  "../../bin/plugins/libexample_udos.pdb"
+  "../../bin/plugins/libexample_udos.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/example_udos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
