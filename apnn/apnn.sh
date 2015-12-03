21c345bd72acf93775302e8ac413b9b1b4d936a4
Suhail Rehman <suhailrehman@gmail.com>
2015-12-03 03:10:10 +0000

    Working APNN Benchmark Script

--- apnn/apnn.sh
+++ apnn/apnn.sh
@@ -3,7 +3,7 @@
 #All-Pairs Nearest Neighbors for N Images already stored in SciDB as an Image Volume
 
 #Rank a maxiumum of MAX_RANK images
-MAX_RANK = 5
+MAX_RANK=5
 
 #Check Command Line Agruments
 if [ $# -eq 0 ]
@@ -12,6 +12,8 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
+#Clear SciDB
+python ../image_average/python/clear_scidb.py
 #Load images from first command-line argument
 ./image_load.py $1
 
@@ -20,4 +22,4 @@ fi
 iquery -tnq "SELECT * INTO image_pairs FROM image_volume as iv1 JOIN image_volume as iv2 ON iv1.i0 = iv2.i0 AND iv1.i1 = iv2.i1 AND iv1.i2 = iv2.i2;"
 
 #APNN Query on Image Cross-Product
-iquery -tnaq "store(knn(aggregate(apply(image_pairs, f0_square, pow(f0-f0_2,2)),sum(f0_square),i3,i3_2), $MAX_K),image_ranks)"
+iquery -tnaq "store(knn(aggregate(apply(image_pairs, f0_square, pow(f0-f0_2,2)),sum(f0_square),i3,i3_2), $MAX_RANK),image_ranks)"
